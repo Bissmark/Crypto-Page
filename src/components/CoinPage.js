5e4e387c099360e1cc9a034d7bc068ad9c2d0ace
Christopher Holt <holt.christopher1@gmail.com>
2022-09-08 18:57:35 +1000

    fiddling with individual coin page css

--- src/components/CoinPage.js
+++ src/components/CoinPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router";
 import moment from 'moment';
-import { Tooltip, YAxis, AreaChart, Area } from "recharts";
+import { Tooltip, YAxis, XAxis, AreaChart, Area, CartesianGrid } from "recharts";
 
 const CoinPage = () => {
     const [coins, setCoins] = useState([]);
@@ -40,33 +40,38 @@ const CoinPage = () => {
     return (
         <div className="coin-container">
             <div className="color-background">
-                <div className="inline-table-text">
-                    <img src={ coins.image.thumb } alt={ coins.name }></img>
-                    <p>Rank: {coins.coingecko_rank}</p>
-                    <p>Name: {coins.name}</p>
-                    <p>Current Price: ${coins.market_data.current_price.usd}</p>
-                    <div className="twenty-four-change">
-                        <p>24hr change: </p>
-                    <p
-                        className={coins.market_data.price_change_percentage_24h > 0 ? "text-success" : "text-danger"}>
-                        { coins.market_data.price_change_percentage_24h.toFixed(2) }%
-                    </p> 
+                <h1 className="title-coin">{coins.name} <img src={ coins.image.thumb } alt={ coins.name }></img></h1>
+                <div className="text-box">
+                    <div className="next-to">
+                        <p>Rank: <span className="blue">{coins.coingecko_rank}</span></p>
+                        <p>Current Price: <span className="blue">${coins.market_data.current_price.usd}</span></p>
+                        <div className="twenty-four-change">
+                            <p>24hr change: </p>
+                        <p
+                            className={coins.market_data.price_change_percentage_24h > 0 ? "text-success" : "text-danger"}>
+                            { coins.market_data.price_change_percentage_24h.toFixed(2) }%
+                        </p> 
+                        </div>
+                        <p>Market Cap: <span className="blue">${coins.market_data.market_cap.usd.toLocaleString()}</span></p>
+                        <p>Circulating Supply: <span className="blue">{coins.market_data.circulating_supply.toLocaleString()}</span></p>
+                        <p>Total Supply: <span className="blue">{coins.market_data.total_supply.toLocaleString()}</span></p>
                     </div>
-                    <p>Market Cap: ${coins.market_data.market_cap.usd.toLocaleString()}</p>
-                    <p>Circulating Supply: {coins.market_data.circulating_supply.toLocaleString()}</p>
-                    <p>Total Supply: {coins.market_data.total_supply.toLocaleString()}</p>
-                    <p>All-Time High: ${coins.market_data.ath.usd.toLocaleString()}</p>
-                    <p>All-Time High Data: {moment(coins.market_data.ath_date.usd).format('Do MMM YY') }</p>
-                    <p>All-Time Low: ${coins.market_data.atl.usd.toLocaleString()}</p>
-                    <p>All-Time Low Data: {moment(coins.market_data.atl_date.usd).format('Do MMM YY')}</p>
+                    <div className="next-to">
+                        <p>All-Time High: <span className="blue">${coins.market_data.ath.usd.toLocaleString()}</span></p>
+                        <p>All-Time High Data: <span className="blue">{moment(coins.market_data.ath_date.usd).format('Do MMM YY') }</span></p>
+                        <p>All-Time Low: <span className="blue">${coins.market_data.atl.usd.toLocaleString()}</span></p>
+                        <p>All-Time Low Data: <span className="blue">{moment(coins.market_data.atl_date.usd).format('Do MMM YY')}</span></p>
+                        <p className="hidden"> sadad</p>
+                        <p className="hidden"> sadsad</p>
+                    </div>
                 </div>
-                <td className="inline-table-text">
-                    <AreaChart width={500} height={300} data={coinPricingData}>
+                    <AreaChart width={960} height={300} data={coinPricingData}>
                         <Area type="monotone" dataKey="price" stroke={priceIncrease ? "#82ca9d" : "red"} fill={priceIncrease ? "#82ca9d" : "red"} dot={false} />
                         <Tooltip content={ Toolip } cursor={ false } wrapperStyle={{ outline: 'none' }} />
-                        <YAxis hide={true} domain={['dataMin', 'dataMax']} />
+                        <YAxis width={80} type="number" tickFormatter={(value) => value.toFixed(2)} domain={['dataMin', 'auto']} />
+                        {/* <XAxis domain={[0, 'auto']} /> */}
+                        <CartesianGrid stroke="#000" strokeDasharray="5 5" />
                     </AreaChart>
-                </td>
             </div>
             
         </div>
